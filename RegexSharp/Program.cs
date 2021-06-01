aa8c3eac7dc8f22cf98d5c5741a89aa1bd279090
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-06-01 13:14:09 +0200

    update

--- RegexSharp/Program.cs
+++ RegexSharp/Program.cs
@@ -1,24 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace RegexSharp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static Dictionary<string, int> ops = new Dictionary<string, int>();
+        private static Dictionary<string, int> r = new Dictionary<string, int>();
+        private static char currentchar;
+        private static string userinput;
+        private static StringBuilder builderl = new StringBuilder();
+        private static StringBuilder builderr = new StringBuilder();
+        public static void Main(string[] args)
         {
-            string userinput;
+            Op();
 
-            do
+            userinput = "b v c*";
+            userinput = RemoveWhitespace(userinput);
+
+
+            int prio = 0;
+            for (int i = 0; i < userinput.Length; i++)
             {
-                Console.Write("Regex: ");
-                userinput = Console.ReadLine();
-                Console.WriteLine(Calc(userinput));
-            } while (true);
+                if(ops.TryGetValue(userinput[i].ToString(), out int num))
+                {
+                    if (prio < num)
+                    {
+                        prio = num;
+                    }
+                }
+
+            }
+            userinput.Split()
+
+            for (int i = 0; i < userinput.Length; i++)
+            {
+                if (ops.TryGetValue(userinput[i].ToString(), out int num))
+                {
+                    
+                }
+                builderl.Append(userinput[i].ToString());
+            }
+
+
+            //if (ops.TryGetValue(s.ToString(), out int val))
+            //{
+            //    if (s=='(')
+            //    {
+            //        builder.Append(s);
+
+            //    }
+
+            //}
+            //switch (s)
+            //{
+
+
+            //    default:
+            //        break;
+            //}
+            //builder.Append(userinput[0]);
+
+
+
+
+            Console.ReadLine();
+            Calc(userinput);
         }
 
         public static string Calc(string reg)
         {
+
+            
+            currentchar = userinput[0];
+            userinput = userinput.Remove(1);
+
+            if (currentchar == '(')
+            {
+
+            }
+            else if (ops.TryGetValue(currentchar.ToString(), out int val) && currentchar != '(')
+            {
+                throw new Exception("invalid input");
+            }
+            else
+            {
+
+            }
+            return "";
+        }
+
+        public static string Rules(string a, string b)
+        {
             throw new NotImplementedException();
         }
+
+        public static void Op()
+        {
+            ops.Add("(", 1);
+            ops.Add(")", 1);
+            ops.Add("+", 100);
+            ops.Add("v", 700);
+            ops.Add("*", 200);
+            ops.Add("⧺", 600);
+
+            
+        }
+
+        public static string RemoveWhitespace(this string input)
+        {
+            return new string(input.ToCharArray()
+                .Where(c => !Char.IsWhiteSpace(c))
+                .ToArray());
+        }
+
+
+
+
     }
 }
